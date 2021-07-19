c2ea5c9f447353a7806d80faebd5acfeb0c2bd5a
Nick Romney <26225537+nickromney@users.noreply.github.com>
2021-07-19 18:10:18 +0100

    Lint generating commit four

--- app/index.js
+++ app/index.js
@@ -11,6 +11,7 @@ const routes = require("./routes");
 // Add a comment to generate a commit
 // Add a second comment where the cache should be present
 // Third commit - don't first set up Node version 14
+// Fourth commit - try fetch-depth:0
 
 // Set up the express app
 const app = express();

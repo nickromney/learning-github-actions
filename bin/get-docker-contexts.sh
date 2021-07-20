f930d2f0cca7db470ec9484300e45007d583a47c
Nick Romney <26225537+nickromney@users.noreply.github.com>
2021-07-20 12:40:55 +0100

    correct paths

--- bin/get-docker-contexts.sh
+++ bin/get-docker-contexts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-declare -a dockercontexts=( "./apps/docker-node-example" "./apps/docker-rails/example")
+declare -a dockercontexts=( "./apps/docker-node-example" "./apps/docker-rails-example")
 printf '%s\n' "${dockercontexts[@]}" | jq -R . | jq -cs .
